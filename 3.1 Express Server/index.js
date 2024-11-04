717651800972330d7f4008398c0ad9c4fb4beccb
kunal nagwanshi <nagwanshikunal23@gmail.com>
2024-11-04 23:58:01 +0530

    express js-2

--- 3.1 Express Server/index.js
+++ 3.1 Express Server/index.js
@@ -1,7 +1,8 @@
 import express from "express";
+
 const app = express();
 const port = 3000;
+app.listen(port,()=>{
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
-});
+  console.log(`server running on port ${port}`);
+})
