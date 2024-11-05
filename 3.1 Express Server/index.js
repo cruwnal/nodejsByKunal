6b483b3e9c382b5d9fe1c65706d6c13ca189c008
kunal nagwanshi <nagwanshikunal23@gmail.com>
2024-11-05 22:07:26 +0530

    setting up the server

--- 3.1 Express Server/index.js
+++ 3.1 Express Server/index.js
@@ -2,7 +2,17 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+
+ app.get("/",(req,res)=>{
+  res.send("<h1>hello i am learning express js</h1>");
+ 
+});
+
+
+
+
+
 app.listen(port,()=>{
 
   console.log(`server running on port ${port}`);
-})
+});
