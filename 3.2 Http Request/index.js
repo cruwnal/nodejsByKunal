ff0a1987ea02dc6ea8c5fcc955bc944889b4265c
kunal nagwanshi <nagwanshikunal23@gmail.com>
2024-11-06 21:53:27 +0530

    creating get request

--- 3.2 Http Request/index.js
+++ 3.2 Http Request/index.js
@@ -5,11 +5,21 @@ const port = 3000;
 app.get("/",(req,res)=>{
     res.send("<h1>i tried this by my self only<h1/> ");
     console.log(req.rawHeaders);
-})
+});
 app.get("/about",(req,res)=>{
     res.send("my name is kunal");
-})
+});
+
+app.get("/contactMe",(req,res)=>{
+    res.send("my contact number is 7999816165");
+});
+
+app.get("/address",(req,res)=>{
+    res.send("bengaluru");
+});
+
 
 app.listen(port,()=>{
     console.log(`server started with port ${port}`);
-})
+});
+
