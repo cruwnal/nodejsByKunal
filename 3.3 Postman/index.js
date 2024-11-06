1019499786cd094cbe27d08726c8deeff92506ff
kunal nagwanshi <nagwanshikunal23@gmail.com>
2024-11-06 23:21:40 +0530

    get post put patch de;ete

---
+++ 3.3 Postman/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/",(req,res)=>{
+    res.send("<h1>I tried this by my self only<h1/> ");
+    console.log(req.rawHeaders);
+});
+app.get("/about",(req,res)=>{
+    res.send("my name is kunal");
+});
+
+app.get("/contactMe",(req,res)=>{
+    res.send("my contact number is 7999816165");
+});
+
+app.get("/address",(req,res)=>{
+    res.send("bengaluru");
+});
+
+app.post("/register",(req,res)=>{
+
+    res.sendStatus(201);
+
+});
+
+app.put("/user/kunal",(req,res)=>{
+    res.sendStatus(200);
+});
+app.patch("/user/kunal",(req,res)=>{
+    res.sendStatus(200);
+});
+
+app.delete("/user/kunal",(req,res)=>{
+    res.sendStatus(200);
+})
+app.listen(port,()=>{
+    console.log(`server started with port ${port}`);
+});
+
